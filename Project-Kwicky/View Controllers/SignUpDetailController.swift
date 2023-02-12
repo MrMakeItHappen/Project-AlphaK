e41075e32618bd9a24a1d4519ffe36abc5b78ce0
Stanley Miller <smiller8203@gmail.com>
2023-02-12 12:48:30 -0500

    - added scrollview to Sign Up Detail Controller - added all ui elements to Sign Up Detail Controller

--- Project-Kwicky/View Controllers/SignUpDetailController.swift
+++ Project-Kwicky/View Controllers/SignUpDetailController.swift
@@ -66,9 +66,29 @@ final class SignUpDetailController: UIViewController, UITextFieldDelegate {
         return label
     }()
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .clear
+        scrollView.delaysContentTouches = false
+        scrollView.clipsToBounds = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+    
+    private let scrollViewContentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 0
+        view.clipsToBounds = true
+        return view
+    }()
+    
     private lazy var fullNameTextField: UITextField = {
         let textField = UITextField(frame: .zero)
-        let placeholder = "ex. Kwiks App"
+        let placeholder = "ex. Mark Blum"
         textField.placeholder = placeholder
         textField.textAlignment = .left
         textField.textColor = .black
@@ -103,6 +123,188 @@ final class SignUpDetailController: UIViewController, UITextFieldDelegate {
         label.textColor = #colorLiteral(red: 0.6392156863, green: 0.6392156863, blue: 0.6392156863, alpha: 1)
         label.width(78)
         return label
+    }()
+    
+    private lazy var emailTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        let placeholder = "ex. kwiks@kwiks.com"
+        textField.placeholder = placeholder
+        textField.textAlignment = .left
+        textField.textColor = .black
+        textField.font = UIFont.interRegular(size: 18)
+        textField.allowsEditingTextAttributes = false
+        textField.autocorrectionType = .no
+        textField.delegate = self
+        textField.backgroundColor = .clear
+        textField.returnKeyType = .next
+        textField.keyboardType = .emailAddress
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 10
+        textField.leftViewMode = .always
+        textField.layer.borderColor = UIColor.borderGrey.cgColor
+        textField.layer.borderWidth = 1
+        textField.clipsToBounds = true
+        textField.layer.masksToBounds = true
+        textField.height(50)
+        textField.setLeftPaddingPoints(30)
+        return textField
+    }()
+    
+    private let emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email ID"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.font = UIFont.interRegular(size: 13)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.textColor = #colorLiteral(red: 0.6392156863, green: 0.6392156863, blue: 0.6392156863, alpha: 1)
+        label.width(78)
+        return label
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        let placeholder = "ex. kwiks#123"
+        textField.placeholder = placeholder
+        textField.textAlignment = .left
+        textField.textColor = .black
+        textField.font = UIFont.interRegular(size: 18)
+        textField.allowsEditingTextAttributes = false
+        textField.autocorrectionType = .no
+        textField.delegate = self
+        textField.backgroundColor = .clear
+        textField.returnKeyType = .next
+        textField.keyboardType = .emailAddress
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 10
+        textField.leftViewMode = .always
+        textField.layer.borderColor = UIColor.borderGrey.cgColor
+        textField.layer.borderWidth = 1
+        textField.clipsToBounds = true
+        textField.layer.masksToBounds = true
+        textField.isSecureTextEntry = true
+        textField.height(50)
+        textField.setLeftPaddingPoints(30)
+        return textField
+    }()
+    
+    private let passwordLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Password"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.font = UIFont.interRegular(size: 13)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.textColor = #colorLiteral(red: 0.6392156863, green: 0.6392156863, blue: 0.6392156863, alpha: 1)
+        label.width(78)
+        return label
+    }()
+    
+    private lazy var birthdateTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        let placeholder = "ex. 01/21/2001"
+        textField.placeholder = placeholder
+        textField.isUserInteractionEnabled = false
+        textField.textAlignment = .left
+        textField.textColor = .black
+        textField.font = UIFont.interRegular(size: 18)
+        textField.allowsEditingTextAttributes = false
+        textField.autocorrectionType = .no
+        textField.delegate = self
+        textField.backgroundColor = .clear
+        textField.returnKeyType = .next
+        textField.keyboardType = .emailAddress
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 10
+        textField.leftViewMode = .always
+        textField.layer.borderColor = UIColor.borderGrey.cgColor
+        textField.layer.borderWidth = 1
+        textField.clipsToBounds = true
+        textField.layer.masksToBounds = true
+        textField.height(50)
+        textField.setLeftPaddingPoints(30)
+        return textField
+    }()
+    
+    private let birthdateLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Date of birth"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .white
+        label.font = UIFont.interRegular(size: 13)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        label.textColor = #colorLiteral(red: 0.6392156863, green: 0.6392156863, blue: 0.6392156863, alpha: 1)
+        label.width(95)
+        return label
+    }()
+    
+    private lazy var datePicker: UIDatePicker = {
+        let datePicker = UIDatePicker(frame: .zero)
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .date
+        datePicker.tintColor = .kwiksGreen
+        datePicker.addTarget(self, action: #selector(handleDateSelection), for: .valueChanged)
+        return datePicker
+    }()
+    
+    private lazy var signUpButton: UIButton = {
+        let icon = UIImage(named: "LoginIcon")
+        var attributeContainer = AttributeContainer()
+        attributeContainer.font = .interRegular(size: 16)
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.cornerStyle = .capsule
+        configuration.image = icon
+        configuration.imagePlacement = .trailing
+        configuration.imagePadding = 10
+        configuration.attributedTitle = AttributedString("Sign Up", attributes: attributeContainer)
+        
+        let button = UIButton(configuration: configuration, primaryAction: UIAction(handler: { _ in
+            self.didTapSignUp()
+        }))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.masksToBounds = true
+        button.backgroundColor = #colorLiteral(red: 0.7764705882, green: 0.7764705882, blue: 0.7764705882, alpha: 1)
+        button.tintColor = .black
+        button.layer.cornerRadius = 25
+        button.height(50)
+        return button
+    }()
+    private let bottomLabel: UILabel = {
+        let label = UILabel()
+        label.text = "I'm already a member,"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.font = UIFont.interRegular(size: 13)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .left
+        label.textColor = UIColor.black
+        return label
+    }()
+    
+    private lazy var signInButton: UIButton = {
+        var attributeContainer = AttributeContainer()
+        attributeContainer.font = .interSemiBold(size: 13)
+        
+        var configuration = UIButton.Configuration.plain()
+        configuration.attributedTitle = AttributedString("Sign in", attributes: attributeContainer)
+        
+        let button = UIButton(configuration: configuration, primaryAction: UIAction(handler: { _ in
+            self.didTapSignIn()
+        }))
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.masksToBounds = true
+        button.backgroundColor = .clear
+        button.tintColor = .kwiksGreen
+        button.sizeToFit()
+        return button
     }()
 
     override func viewDidLoad() {
@@ -156,13 +358,90 @@ extension SignUpDetailController {
         self.detailsLabel.topToSuperview(offset: 42)
         self.detailsLabel.leftToSuperview(offset: 40)
         
-        self.loginContainerView.addSubview(self.fullNameTextField)
-        self.fullNameTextField.topToBottom(of: self.detailsLabel, offset: 34)
-        self.fullNameTextField.leftToSuperview(offset: 40)
-        self.fullNameTextField.rightToSuperview(offset: -40)
+        self.loginContainerView.addSubview(self.scrollView)
+        self.scrollView.topToBottom(of: self.detailsLabel, offset: 34)
+        self.scrollView.leftToSuperview(offset: 40)
+        self.scrollView.rightToSuperview(offset: -40)
+        self.scrollView.bottomToSuperview(offset: -20)
         
-        self.loginContainerView.addSubview(self.fullNameLabel)
+        self.scrollView.addSubview(self.scrollViewContentView)
+        self.scrollViewContentView.edgesToSuperview()
+        self.scrollViewContentView.width(to: self.scrollView)
+//        self.scrollViewContentView.height(1000)
+        
+        self.scrollViewContentView.addSubview(self.fullNameTextField)
+        self.fullNameTextField.topToSuperview(offset: 10)
+        self.fullNameTextField.leftToSuperview()
+        self.fullNameTextField.rightToSuperview()
+
+        self.scrollViewContentView.addSubview(self.fullNameLabel)
         self.fullNameLabel.top(to: self.fullNameTextField, offset: -8)
         self.fullNameLabel.left(to: self.fullNameTextField, offset: 22)
+
+        self.scrollViewContentView.addSubview(self.emailTextField)
+        self.emailTextField.topToBottom(of: self.fullNameTextField, offset: 30)
+        self.emailTextField.left(to: self.fullNameTextField)
+        self.emailTextField.right(to: self.fullNameTextField)
+
+        self.scrollViewContentView.addSubview(self.emailLabel)
+        self.emailLabel.top(to: self.emailTextField, offset: -8)
+        self.emailLabel.left(to: self.emailTextField, offset: 18)
+
+        self.scrollViewContentView.addSubview(self.passwordTextField)
+        self.passwordTextField.topToBottom(of: self.emailTextField, offset: 30)
+        self.passwordTextField.left(to: self.fullNameTextField)
+        self.passwordTextField.right(to: self.fullNameTextField)
+
+        self.scrollViewContentView.addSubview(self.passwordLabel)
+        self.passwordLabel.top(to: self.passwordTextField, offset: -8)
+        self.passwordLabel.left(to: self.passwordTextField, offset: 22)
+
+        self.scrollViewContentView.addSubview(self.birthdateTextField)
+        self.birthdateTextField.topToBottom(of: self.passwordTextField, offset: 30)
+        self.birthdateTextField.left(to: self.fullNameTextField)
+        self.birthdateTextField.right(to: self.fullNameTextField)
+
+        self.scrollViewContentView.addSubview(self.birthdateLabel)
+        self.birthdateLabel.top(to: self.birthdateTextField, offset: -8)
+        self.birthdateLabel.left(to: self.birthdateTextField, offset: 22)
+        
+        self.scrollViewContentView.addSubview(self.datePicker)
+        self.datePicker.right(to: self.birthdateTextField, offset: -10)
+        self.datePicker.centerY(to: self.birthdateTextField)
+        
+        self.scrollViewContentView.addSubview(self.signUpButton)
+        self.signUpButton.topToBottom(of: self.birthdateTextField, offset: 37)
+        self.signUpButton.left(to: self.fullNameTextField)
+        self.signUpButton.right(to: self.fullNameTextField)
+        
+        self.scrollViewContentView.addSubview(self.bottomLabel)
+        self.bottomLabel.topToBottom(of: self.signUpButton, offset: 43)
+        self.bottomLabel.centerXToSuperview(offset: -45)
+        
+        self.scrollViewContentView.addSubview(self.signInButton)
+        self.signInButton.top(to: self.bottomLabel)
+        self.signInButton.bottom(to: self.bottomLabel)
+        self.signInButton.leftToRight(of: self.bottomLabel, offset: -6)
+        
+        self.scrollViewContentView.bottom(to: self.bottomLabel, offset: 4)
+    }
+}
+//MARK: - @objc
+extension SignUpDetailController {
+    @objc func didTapSignUp() {
+        print(#function)
+    }
+    
+    @objc func didTapSignIn() {
+        print(#function)
+    }
+    
+    @objc func handleDateSelection() {
+        //Remember to upload in a different format.
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        
+        self.birthdateTextField.text = dateFormatter.string(from: self.datePicker.date)
+        self.view.endEditing(true)
     }
 }

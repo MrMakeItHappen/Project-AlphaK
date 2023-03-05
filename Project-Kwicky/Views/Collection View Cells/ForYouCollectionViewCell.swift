bdb21384404ccea085ddd5c15854f2f2e1381b12
Stanley Miller <smiller8203@gmail.com>
2023-03-05 10:27:17 -0500

    - modified For You Cell - added icon asset

--- Project-Kwicky/Views/Collection View Cells/ForYouCollectionViewCell.swift
+++ Project-Kwicky/Views/Collection View Cells/ForYouCollectionViewCell.swift
@@ -61,6 +61,45 @@ final class ForYouCollectionViewCell: UICollectionViewCell {
         return imageView
     }()
     
+    private let viewCountContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        view.backgroundColor = UIColor(hexString: "#8C8687", alpha: 0.80)
+        view.layer.cornerRadius = 6
+        view.height(20)
+        view.width(50)
+        return view
+    }()
+    
+    private let viewCountIconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .clear
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .clear
+        imageView.layer.masksToBounds = true
+        imageView.clipsToBounds = true
+        imageView.height(9)
+        imageView.width(9)
+        
+        let image = UIImage(named: "ViewersIcon")
+        imageView.image = image
+        return imageView
+    }()
+    
+    private let viewCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        label.font = UIFont.segoeUISemiBold(size: 10)
+        label.numberOfLines = 1
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .left
+        label.textColor = UIColor.white
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.layer.cornerRadius = 10
@@ -75,6 +114,12 @@ final class ForYouCollectionViewCell: UICollectionViewCell {
         self.videoPreviewImageView.image = item.deleteLaterImage
         self.uploadedByLabel.text = item.uploadedFrom.userName
         self.titleLabel.text = item.title
+        
+        guard let videoViewCount = item.viewers else {
+            self.viewCountLabel.text = "🔥🔥🔥"
+            return
+        }
+        self.viewCountLabel.text = "\(videoViewCount)"
     }
     
     private func layoutUI() {
@@ -92,6 +137,18 @@ final class ForYouCollectionViewCell: UICollectionViewCell {
         self.titleLabel.left(to: self.uploadedByLabel)
         self.titleLabel.bottomToTop(of: self.uploadedByLabel, offset: -2)
         self.titleLabel.rightToSuperview(offset: -14)
+        
+        self.addSubview(self.viewCountContainerView)
+        self.viewCountContainerView.topToSuperview(offset: 8)
+        self.viewCountContainerView.rightToSuperview(offset: -9)
+        
+        self.viewCountContainerView.addSubview(self.viewCountIconImageView)
+        self.viewCountIconImageView.leftToSuperview(offset: 7)
+        self.viewCountIconImageView.centerYToSuperview()
+        
+        self.viewCountContainerView.addSubview(self.viewCountLabel)
+        self.viewCountLabel.centerYToSuperview()
+        self.viewCountLabel.leftToRight(of: self.viewCountIconImageView, offset: 4)
+        self.viewCountLabel.rightToSuperview(offset: -7)
     }
 }
-#warning("Work here. Need to add view count to cell. Then other icons on Home Controller. Dueces")
